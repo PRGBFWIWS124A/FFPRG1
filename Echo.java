690b1aa377b37ce77a6ab74d3720715a1dca3f56
FlorianFHDW <florian.foerster1@edu.fhdw.de>
2024-01-12 12:14:28 +0100

    Edit Sum.java

--- Echo.java
+++ Echo.java
@@ -1,6 +1,5 @@
 public class Echo {
     public static void main(String[] args) {
-        System.out.println("Parameter: "+args[0]);
-        System.out.println("Hello World");
+        System.out.println(args[0]);
     }
 }
--- Sum.java
+++ Sum.java
@@ -1,12 +1,16 @@
 public class Sum {
     public static void main(String[] args) {
-        try {
-            int a1 = Integer.parseInt(args[0]);
-            int a2 = Integer.parseInt(args[1]);
+        if (args.length > 0) {
+            try {
+                int a1 = Integer.parseInt(args[0]);
+                int a2 = Integer.parseInt(args[1]);
 
-            System.out.println("Summe: "+(a1+a2));
-        } catch (NumberFormatException ignored) {
-            System.out.println("Argumente müssen Zahlen enthalten");
+                System.out.println("Summe: " + (a1 + a2));
+            } catch (NumberFormatException ignored) {
+                System.err.println("Argumente müssen Zahlen enthalten");
+            }
+        } else {
+            System.err.println("Keine Argumente angegeben");
         }
     }
 }
