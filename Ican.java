d760a0c0f04ec918a61b3bcc59cd3d362a43d0ed
FlorianFHDW <florian.foerster1@edu.fhdw.de>
2024-01-19 12:58:02 +0100

    Add Math.java

--- Ican.java
+++ Ican.java
@@ -1,5 +1,7 @@
 public class Ican {
     public static void main(String[] args) {
-        System.out.println("Ich kann programmieren!");
+        int x = 120; int y = 2; int z = 3;
+        Object o = (x != 'x' ? 2f : 1);
+        System.out.println(o.getClass().getName().split("\\.")[2] + " " + o);
     }
 }
---
+++ Math.java
@@ -0,0 +1,18 @@
+public class Math {
+    public static void main(String[] args) {
+
+    }
+
+    static int product(int x, int y) {
+        return x * y;
+    }
+    static int squaresum(int x, int y) {
+        return x * x + y * y;
+    }
+    static void output(String content) {
+        System.out.println(content);
+    }
+    static void warning() {
+        System.out.println("WARNUNG");
+    }
+}
